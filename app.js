502c1505dab399d5dd4d43769b0e2a6e4f356dea
Dylane <tanolamene@gmail.com>
2024-12-30 09:07:08 -0400

    Mise ajour du middleware cors

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Configurer CORS
 app.use(cors({
-  origin: "https://carnet-adresses.vercel.app/" || "http://127.0.0.1:5500", // Adresse du client
+  origin: "https://carnet-adresses.vercel.app" || "http://127.0.0.1:5500", // Adresse du client
   methods: ["GET", "POST", "PUT", "DELETE"], // Méthodes autorisées
   allowedHeaders: ["Content-Type", "Authorization"], // Headers autorisés
   credentials: true, // Autorise les cookies ou les headers sécurisés
