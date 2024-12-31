7d16f20bc490522816800c75d282deb8479652cb
Dylane <tanolamene@gmail.com>
2024-12-30 22:17:25 -0400

    Implementation du logout13-mm si ca fonctionne deja

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -141,32 +141,34 @@ const refreshToken = async (req, res) => {
   }
 };
 
-// Ajoute cette route dans ton controller pour la déconnexion
 const logoutUser = async (req, res) => {
   try {
-    res.cookie("accessToken", "", {
+    // Supprime le cookie accessToken
+    res.clearCookie("accessToken", {
       httpOnly: true,
       secure: NODE_ENV === "production",
-      sameSite: "None",
-      domain: "carnet-adresses-50e2ff3ffe95.herokuapp.com",
-      maxAge: 0, // Expire immédiatement
+      sameSite: "none",
+      domain: "carnet-adresses.vercel.app", // Assure-toi que le domaine correspond à celui défini pour les cookies
     });
 
-    res.cookie("refreshToken", "", {
+    // Supprime le cookie refreshToken
+    res.clearCookie("refreshToken", {
       httpOnly: true,
       secure: NODE_ENV === "production",
-      sameSite: "None",
-      domain: "carnet-adresses-50e2ff3ffe95.herokuapp.com",
-      maxAge: 0, // Expire immédiatement
+      sameSite: "none",
+      domain: "carnet-adresses.vercel.app", // Assure-toi que le domaine correspond à celui défini pour les cookies
     });
-    res.clearCookie("accessToken");
-    res.clearCookie("refreshToken");
 
     res.status(200).json({ success: true, message: "Déconnexion réussie." });
   } catch (err) {
-    res.status(500).json({ success: false, message: "Erreur lors de la déconnexion.", error: err.message });
+    res.status(500).json({
+      success: false,
+      message: "Erreur lors de la déconnexion.",
+      error: err.message,
+    });
   }
 };
+
 
 
 module.exports = {
